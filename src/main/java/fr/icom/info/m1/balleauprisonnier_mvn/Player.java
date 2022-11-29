de611a88b7831544cbc14f9674cce82c3b4a58cc
TMayling <ntang@free.fr>
2022-11-29 12:11:48 +0100

    Nouvelle version

--- src/main/java/fr/icom/info/m1/balleauprisonnier_mvn/Player.java
+++ src/main/java/fr/icom/info/m1/balleauprisonnier_mvn/Player.java
@@ -9,6 +9,7 @@ import javafx.scene.transform.Rotate;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.util.Duration;
+import java.util.Random;
 
 /**
  * 
@@ -69,11 +70,11 @@ public class Player
         //directionArrow = sprite.getClip().;
 
 	    // Tous les joueurs ont une vitesse aleatoire entre 0.0 et 1.0
-        // Random randomGenerator = new Random();
-        // step = randomGenerator.nextFloat();
+        Random randomGenerator = new Random();
+        step = randomGenerator.nextFloat();
 
-        // Pour commencer les joueurs ont une vitesse / un pas fixe
-        step = 1;
+        // Vitesse / un pas fixe
+        //step = 1;
 	    
 	  }
 
