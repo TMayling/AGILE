fda62271f8df13dbe0eda94af2543620109430f5
Clara <63711079+ClaraMorais@users.noreply.github.com>
2022-11-29 13:57:17 +0100

    Mise à jour Projectile

--- src/main/java/fr/icom/info/m1/balleauprisonnier_mvn/Projectile.java
+++ src/main/java/fr/icom/info/m1/balleauprisonnier_mvn/Projectile.java
@@ -1,6 +1,39 @@
-package fr.icom.info.m1.balleauprisonnier_mvn;
+//package fr.icom.info.m1.balleauprisonnier_mvn;
 
-public class Projectile {
+//import javafx.scene.canvas.GraphicsContext;
+//import javafx.scene.image.Image;
+//import javafx.scene.image.ImageView;
+//import javafx.scene.transform.Rotate;
+
+//public class Projectile {
 	
+	//Image Balle;
+	//GraphicsContext graphicsContext;
+	//double x;       // position horizontale du joueur
+	 // final double y; 	  // position verticale du joueur
+	 // double angle = 90; // rotation du joueur, devrait toujour être en 0 et 180
+	 // double step;    // pas d'un joueur
+	 // String playerColor;
+	  
+	  // On une image globale du joueur 
+	 // Image directionArrow;
+	 // Sprite sprite;
+	 // ImageView PlayerDirectionArrow;
+	  
+	
+	//void display()
+	 // {
+		//  graphicsContext.save(); // saves the current state on stack, including the current transform
+		//  graphicsContext.drawImage(directionArrow, x, y);
+		//  graphicsContext.restore(); // back to original state (before rotation)
+	 // }
+	
+	//void AffichageBalle(GraphicsContext gc){
+    //	Balle = new Image("assets/ball.png");
+    //	Balle.display();
+	//};
+	
+	
+	
+//}
 
-}
